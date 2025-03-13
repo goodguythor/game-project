db5db230f6e01c0be869e2b154d9cd14dc23bd14
Thoriqutsani Anastyo Rasnan <thoriq.rasnan@gmail.com>
2025-03-13 22:22:07 +0700

    implement game over

--- catur-abjad/main.cpp
+++ catur-abjad/main.cpp
@@ -3,6 +3,8 @@
 #include <string>
 #include <iostream>
 
+// TODO: IMPLEMENT GAME LOGIC
+
 static std::vector<std::vector<int>> grid(8,std::vector<int>(8));
 static std::vector<int> clickedPiece{-1,-1};
 static const int screenWidth = 720;
@@ -10,6 +12,7 @@ static const int screenHeight = 720;
 static int move = 0;
 static bool runWindow = true;
 static bool clicked = false;
+static bool gameOver = false;
 
 static bool validGrid(int x,int y){
     return x>=0&&x<8&&y>=0&&y<8;
@@ -46,12 +49,21 @@ static void init(){
 }
 
 static void draw(){
+    if(gameOver){
+        Vector2 textPos;
+        Vector2 textSize=MeasureTextEx(GetFontDefault(),"Game Over",30,1);
+        Color color=BLACK;
+        float centerX=360,centerY=60;
+        textPos.x=centerX-(textSize.x/2);
+        textPos.y=centerY-(textSize.y/2);
+        DrawTextEx(GetFontDefault(),"Game Over",textPos,30,1,color);
+        // DrawText("Game Over",0,0,64,BLACK);
+    }
     if(clicked){
         int x=clickedPiece[0],y=clickedPiece[1];
         int piece=grid[x][y]%5;
         int posX=120+x*60,posY=120+y*60;
         bool side=grid[x][y]/5;
-
         DrawRectangle(posX,posY,60,60,WHITE);
         if(piece==1){
             if(x>0&&grid[x-1][y]/5!=side) DrawRectangle(posX-60,posY,60,60,WHITE);
@@ -165,7 +177,6 @@ static void draw(){
     for(int i=0;i<8;i++){
         for(int j=0;j<8;j++){
             if(grid[i][j]==10) continue;
-            Vector2 textPos;
             Color color=clicked&&clickedPiece==std::vector<int>{i,j}?BLUE:BLACK;
             if(grid[i][j]/5) color=clicked&&clickedPiece==std::vector<int>{i,j}?GREEN:GRAY;
             char* c;
@@ -177,6 +188,7 @@ static void draw(){
             else if(piece==3) c="T";
             else c="U";
             // std::cout<<grid[i][j]<<'\n';
+            Vector2 textPos;
             Vector2 textSize=MeasureTextEx(GetFontDefault(),c,30,1);
             textPos.x=centerX-(textSize.x/2);
             textPos.y=centerY-(textSize.y/2);
@@ -186,131 +198,142 @@ static void draw(){
 }
 
 static void update(){
+    if(WindowShouldClose()||IsKeyPressed(KEY_ESCAPE)) runWindow = 0;
     Vector2 mousePos = GetMousePosition();
     if(IsMouseButtonPressed(MOUSE_LEFT_BUTTON)){
-        int mouseX=(mousePos.x - 120)/60;
-        int mouseY=(mousePos.y - 120)/60;
-        if(validGrid(mouseX,mouseY)){
-            int piece=grid[mouseX][mouseY];
-            // std::cout<<piece<<'\n';
-            if(!clicked&&piece!=10){
-                bool side=grid[mouseX][mouseY]/5;
-                // std::cout<<move<<' '<<side<<'\n';
-                if((move&1)==side){
-                    clicked=1;
-                    clickedPiece={mouseX,mouseY};    
-                }
-            }
-            else if(clicked){
-                int x=clickedPiece[0],y=clickedPiece[1];
-                int piece=grid[x][y]%5;
-                bool side=grid[x][y]/5;
-                bool good=0;
-                if(piece==1){
-                    if(abs(mouseX-x)+abs(mouseY-y)==1&&grid[mouseX][mouseY]/5!=side){
-                        grid[mouseX][mouseY]=grid[x][y]; 
-                        good=1;
+        if(gameOver){
+            init();
+            clicked=false;
+            gameOver=false;
+        }
+        else{
+            int mouseX=(mousePos.x - 120)/60;
+            int mouseY=(mousePos.y - 120)/60;
+            if(validGrid(mouseX,mouseY)){
+                int piece=grid[mouseX][mouseY];
+                // std::cout<<piece<<'\n';
+                if(!clicked&&piece!=10){
+                    bool side=grid[mouseX][mouseY]/5;
+                    // std::cout<<move<<' '<<side<<'\n';
+                    if((move&1)==side){
+                        clicked=1;
+                        clickedPiece={mouseX,mouseY};    
                     }
                 }
-                else if(piece==0){
-                    if(abs(mouseX-x)==1&&abs(mouseY-y)==1&&grid[mouseX][mouseY]/5!=side){
-                        grid[mouseX][mouseY]=grid[x][y]; 
-                        good=1;
-                    }
-                } 
-                else if(piece==2){
-                    int difX=abs(mouseX-x),difY=abs(mouseY-y);
-                    if(difX==difY&&grid[mouseX][mouseY]/5!=side){
-                        bool check=1;
-                        for(int i=1;i<difX;i++){
-                            if(mouseX>x){
-                                if(mouseY>y){
-                                    if(grid[x+i][y+i]!=10){
-                                        check=0;
-                                        break;
-                                    }
-                                }
-                                else{
-                                    if(grid[x+i][y-i]!=10){
-                                        check=0;
-                                        break;
-                                    }
-                                }
-                            }
-                            else{
-                                if(mouseY>y){
-                                    if(grid[x-i][y+i]!=10){
-                                        check=0;
-                                        break;
-                                    }
-                                }
-                                else{
-                                    if(grid[x-i][y-i]!=10){
-                                        check=0;
-                                        break;
-                                    }
-                                }
-                            }
-                        }
-                        if(check){
+                else if(clicked){
+                    int x=clickedPiece[0],y=clickedPiece[1];
+                    int piece=grid[x][y]%5,enemy=grid[mouseX][mouseY];;
+                    bool side=grid[x][y]/5;
+                    bool good=0;
+                    if(piece==1){
+                        if(abs(mouseX-x)+abs(mouseY-y)==1&&enemy/5!=side){
                             grid[mouseX][mouseY]=grid[x][y]; 
                             good=1;
                         }
                     }
-                } 
-                else if(piece==3){
-                    int difX=abs(mouseX-x),difY=abs(mouseY-y);
-                    int mx=std::max(difX,difY),mn=std::min(difX,difY);
-                    if(mx>0&&mn==0&&grid[mouseX][mouseY]/5!=side){
-                        bool check=1;
-                        for(int i=1;i<mx;i++){
-                            if(mx==difX){
+                    else if(piece==0){
+                        if(abs(mouseX-x)==1&&abs(mouseY-y)==1&&enemy/5!=side){
+                            grid[mouseX][mouseY]=grid[x][y]; 
+                            good=1;
+                        }
+                    } 
+                    else if(piece==2){
+                        int difX=abs(mouseX-x),difY=abs(mouseY-y);
+                        if(difX==difY&&enemy/5!=side){
+                            bool check=1;
+                            for(int i=1;i<difX;i++){
                                 if(mouseX>x){
-                                    if(grid[x+i][y]!=10){
-                                        check=0;
-                                        break;
+                                    if(mouseY>y){
+                                        if(grid[x+i][y+i]!=10){
+                                            check=0;
+                                            break;
+                                        }
+                                    }
+                                    else{
+                                        if(grid[x+i][y-i]!=10){
+                                            check=0;
+                                            break;
+                                        }
                                     }
                                 }
                                 else{
-                                    if(grid[x-i][y]!=10){
-                                        check=0;
-                                        break;
+                                    if(mouseY>y){
+                                        if(grid[x-i][y+i]!=10){
+                                            check=0;
+                                            break;
+                                        }
+                                    }
+                                    else{
+                                        if(grid[x-i][y-i]!=10){
+                                            check=0;
+                                            break;
+                                        }
                                     }
                                 }
                             }
-                            else{
-                                if(mouseY<y){
-                                    if(grid[x][y-i]!=10){
-                                        check=0;
-                                        break;
-                                    }
-                                }
-                                else{
-                                    if(grid[x][y+i]!=10){
-                                        check=0;
-                                        break;
-                                    }
-                                }
+                            if(check){
+                                grid[mouseX][mouseY]=grid[x][y]; 
+                                good=1;
                             }
                         }
-                        if(check){
+                    } 
+                    else if(piece==3){
+                        int difX=abs(mouseX-x),difY=abs(mouseY-y);
+                        int mx=std::max(difX,difY),mn=std::min(difX,difY);
+                        if(mx>0&&mn==0&&enemy/5!=side){
+                            bool check=1;
+                            for(int i=1;i<mx;i++){
+                                if(mx==difX){
+                                    if(mouseX>x){
+                                        if(grid[x+i][y]!=10){
+                                            check=0;
+                                            break;
+                                        }
+                                    }
+                                    else{
+                                        if(grid[x-i][y]!=10){
+                                            check=0;
+                                            break;
+                                        }
+                                    }
+                                }
+                                else{
+                                    if(mouseY<y){
+                                        if(grid[x][y-i]!=10){
+                                            check=0;
+                                            break;
+                                        }
+                                    }
+                                    else{
+                                        if(grid[x][y+i]!=10){
+                                            check=0;
+                                            break;
+                                        }
+                                    }
+                                }
+                            }
+                            if(check){
+                                grid[mouseX][mouseY]=grid[x][y]; 
+                                good=1;
+                            }   
+                        }
+                    }
+                    else{
+                        if((abs(mouseX-x)<=1)&&(abs(mouseY-y)<=1)&&grid[mouseX][mouseY]/5!=side){
                             grid[mouseX][mouseY]=grid[x][y]; 
                             good=1;
-                        }   
+                        }
                     }
-                }
-                else{
-                    if((abs(mouseX-x)<=1)&&(abs(mouseY-y)<=1)&&grid[mouseX][mouseY]/5!=side){
-                        grid[mouseX][mouseY]=grid[x][y]; 
-                        good=1;
+                    if(good){
+                        if(enemy%5==4){
+                            gameOver=1;
+                        }
+                        grid[x][y]=10;
+                        move++;
                     }
+                    clicked=0;
+                    clickedPiece={-1,-1};
                 }
-                if(good){
-                    grid[x][y]=10;
-                    move++;
-                }
-                clicked=0;
-                clickedPiece={-1,-1};
             }
         }
     }
